08bf5251bf858cd3a0478ac90d0090762393543a
whitefurlin <whitefurlin@gamasys.com.tw>
2017-03-08 18:12:25 +0800

    query member picture and send via websocket

--- client.js
+++ client.js
@@ -1,10 +1,39 @@
 const WebSocket = require('ws');
 
+//const ws = new WebSocket('ws://192.168.122.162:8090/');
 const ws = new WebSocket('ws://localhost:8090/');
 
-ws.on('open', function open() {
-  ws.send('whitefur from client');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '192.168.122.174',
+  user     : 'root',
+  password : 'gama.net',
+  database : 'egaming'
 });
+
+//connection.connect();
+
+ws.on('open', function open() {
+  
+  //ws.send('whitefur from client');
+  //setInterval(() => {
+  //  ws.send('what a fucking job!!!! damn it!!!', err => {
+  //    console.log('err');
+  //    console.log(err);
+  //  });
+  //}, 1000);
+  connection.query('SELECT Picture FROM Member WHERE Picture IS NOT NULL LIMIT 1', function (error, results, fields) {
+    if (error) throw error;
+    var pic = results[0]['Picture'];
+    var string = pic.toString('hex');
+    setInterval(() => {
+      ws.send(string);
+      console.log('send hex');
+    }, 3000);
+    connection.end();
+  });
+});
+
 
 ws.on('message', function incoming(data, flags) {
   // flags.binary will be set if a binary data is received.
